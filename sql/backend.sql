ded3b56743f19b68f36658edf74e9de401e3769f
Mikkel Christiansen <mikkel@rheosystems.com>
2012-06-02 15:49:19 +0200

    added NOT NULL and indexes

--- sql/backend.sql
+++ sql/backend.sql
@@ -5,25 +5,27 @@ USE caralert;
 CREATE TABLE  UserTbl
 (
     UserID  integer Unsigned Auto_Increment primary key,
-    UserName Varchar(20),
-    PassWord Varchar(10),
+    UserName Varchar(20) NOT NULL,
+    PassWord Varchar(10) NOT NULL,
     FirstName Varchar(25),
     Surname Varchar(25),
-    CellNumber Varchar(10),   
-    Role varchar(20)
+    CellNumber Varchar(10),
+    Role SET('user','moderator','admin') NOT NULL DEFAULT 'user',
+    UNIQUE (UserName),
+    KEY idx_username (username)
 );
 
 CREATE TABLE  GroupTbl
 (
     GroupID  integer unsigned primary key,    
-    Area varchar(30),
+    Area varchar(30) NOT NULL,
     Description varchar(100)
 );
 
-CREATE TABLE  UserGrgrouptbloupTbl
+CREATE TABLE  UserGroupTbl
 (
-    UserID  integer unsigned,    
-    GroupID integer unsigned,
+    UserID  integer unsigned NOT NULL,
+    GroupID integer unsigned NOT NULL,
     FOREIGN KEY (UserID) REFERENCES UserTbl (UserID) ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY (GroupID) REFERENCES GroupTbl (GroupID) ON UPDATE CASCADE ON DELETE CASCADE
 );
@@ -31,11 +33,12 @@ CREATE TABLE  UserGrgrouptbloupTbl
 CREATE TABLE  IncidentTbl
 (
     IncID integer Unsigned Auto_Increment primary key,
-    UserId integer unsigned,
-    CarRegNo  varchar(10),    
+    UserId integer unsigned NOT NULL,
+    CarRegNo  varchar(10),
     CarColor varchar(30),
     CarMakeModel varchar(30),
     IncDesc varchar(100),
-    IncDate dateTime,
-    FOREIGN KEY (UserID) REFERENCES UserTbl (UserID) ON UPDATE CASCADE
+    IncDate dateTime NOT NULL,
+    FOREIGN KEY (UserID) REFERENCES UserTbl (UserID) ON UPDATE CASCADE,
+    KEY idx_CarRegNo (CarRegNo)
 );
