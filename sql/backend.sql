08fb034ad0b145f05fdddfe23d4cb6724f9d73a1
unknown <RevyTox@RevyTox-PC.(none)>
2012-06-02 15:06:23 +0200

    corrected errors

--- sql/backend.sql
+++ sql/backend.sql
@@ -1,23 +1,6 @@
-CREATE TABLE  IncidentTbl
-(
-    IncID integer Unsigned Auto_Increment primary key,
-    UserId integer unsigned,
-    CarRegNo  varchar(10),    
-    CarColor varchar(30),
-    CarMakeModel varchar(30),
-    IncDesc varchar(100),
-    IncDate dateTime,
-    FOREIGN KEY (UserID) REFERENCES UserTbl (UserId) ON UPDATE CASCADE
-)
-
-
-CREATE TABLE  UserGroupTbl
-(
-    UserID  integer unsigned,    
-    GroupID integer unsigned,
-    FOREIGN KEY (UserId) REFERENCES UserTbl (UserId) ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY (GroupId) REFERENCES GroupTbl (GroupId) ON UPDATE CASCADE ON DELETE CASCADE
-)
+DROP DATABASE IF EXISTS caralert;
+CREATE DATABASE caralert;
+USE caralert;
 
 CREATE TABLE  UserTbl
 (
@@ -28,11 +11,31 @@ CREATE TABLE  UserTbl
     Surname Varchar(25),
     CellNumber Varchar(10),   
     Role varchar(20)
-)
+);
 
 CREATE TABLE  GroupTbl
 (
-    UserID  integer primary key,    
+    GroupID  integer unsigned primary key,    
     Area varchar(30),
     Description varchar(100)
-)
+);
+
+CREATE TABLE  UserGrgrouptbloupTbl
+(
+    UserID  integer unsigned,    
+    GroupID integer unsigned,
+    FOREIGN KEY (UserID) REFERENCES UserTbl (UserID) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (GroupID) REFERENCES GroupTbl (GroupID) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE  IncidentTbl
+(
+    IncID integer Unsigned Auto_Increment primary key,
+    UserId integer unsigned,
+    CarRegNo  varchar(10),    
+    CarColor varchar(30),
+    CarMakeModel varchar(30),
+    IncDesc varchar(100),
+    IncDate dateTime,
+    FOREIGN KEY (UserID) REFERENCES UserTbl (UserID) ON UPDATE CASCADE
+);
