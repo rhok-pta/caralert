2360511f85cb42f42b8188082538bc2c74fe9321
unknown <Biyi@SATURN.(none)>
2012-06-02 14:46:26 +0200

    stable... fixed json

--- php/index.php
+++ php/index.php
@@ -2,8 +2,10 @@
 include_once 'data/db.php';
 
 include_once 'request/get.php';
-include_once 'request/json.php';
-
+if (isset($_REQUEST['json'])){
+	include_once 'request/json.php';
+	exit;
+}
 include_once 'forms/header.html';
 include_once 'forms/nav.html';
 include_once 'forms/index.php';
