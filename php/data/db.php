48c5ccc9862d56ddfdde6235d4241ecf33a335f1
Biyi Akinpelu <biyi@jupiter.entilda.com>
2012-06-03 01:39:11 +0200

    added json query feed (e.g. url query request should be like: caralert/php/?q=list_user         ...  to get all user list data caralert/php/?q=list_incident     ...  to get all incident list data caralert/php/?q=list_group        ...  to get all group list data caralert/php/?q=list_user_group   ...  to get all user group list data

--- php/data/db.php
+++ php/data/db.php
@@ -17,7 +17,74 @@ $connection = yaml_parse_file($yaml_file);
 
 try {
   $dns = 'mysql:host=' . $connection['db_host'] . ';dbname=' .    $connection['db_name'];
-  $pdo = new PDO($dns, $connection['db_user'], $connection['db_pass']);
+  $dbh = new PDO($dns, $connection['db_user'], $connection['db_pass']);
 } catch (PDOEXception $e){
   die('Connection failed: ' . $e->getMessage())."\n";
+}
+
+$dbh->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+
+/**
+ * database helper functions
+ */
+
+
+/**
+ * list all users in the user table
+ * @return array $result An array list of all users
+ */
+function list_user(){
+  global $dbh;
+  $query = "SELECT * FROM UserTbl";
+  $sth = $dbh->prepare($query);
+  $sth->execute();
+  $result = $sth->fetchAll();
+  return $result;
+}
+
+function list_group(){
+  global $dbh;
+  $query = "SELECT * FROM GroupTbl";
+  $sth = $dbh->prepare($query);
+  $sth->execute();
+  $result = $sth->fetchAll();
+  return $result;
+}
+
+function list_user_group(){
+  global $dbh;
+  $query = "SELECT * FROM UserGroupTbl";
+  $sth = $dbh->prepare($query);
+  $sth->execute();
+  $result = $sth->fetchAll();
+  return $result;
+}
+
+function list_incident(){
+  global $dbh;
+  $query = "SELECT * FROM IncidentTbl";
+  $sth = $dbh->prepare($query);
+  $sth->execute();
+  $result = $sth->fetchAll();
+  return $result;
+}
+
+function get_user(){
+}
+
+
+function count_user(){
+  return count(list_user());
+}
+
+function count_group(){
+  return count(list_group());
+}
+
+function count_user_group(){
+  return count(list_user_group());
+}
+
+function count_incident(){
+  return count(list_incident());
 }
--- php/request/get.php
+++ php/request/get.php
@@ -1,4 +1,28 @@
 <?php
 
-$uid = $_REQUEST['uid'];
-$gid = $_REQUEST['gid'];
+$request = $_REQUEST;
+
+$request = sanitize($request);
+
+$query = $request['q'];
+
+
+
+switch($query){
+  case 'list_user':
+    $data = list_user();
+    break;
+  case 'list_group':
+    $data = list_group();
+    break;
+  case 'list_user_group':
+    $data = list_user_group();
+    break;
+  case 'list_incident':
+    $data = list_incident();
+    break;
+  default:
+    // not a valid request
+    $data = false;
+    return;
+}
