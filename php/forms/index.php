dbb38f81e17119edf951270909dba5b424616911
unknown <Biyi@SATURN.(none)>
2012-06-02 14:42:57 +0200

    stable...

--- php/forms/index.php
+++ php/forms/index.php
@@ -1,22 +1,28 @@
 <?php
 global $_GET;
-global $_POST;
+
 $_REQUEST = array_merge($_GET, $_POST);
-//var_dump($_POST);
+
 $action = $_REQUEST['action'];
+
+@session_start();
+
+$action = $_GET['action'];
+
 switch($action){
 	case 'adduser':
+		include_once 'auth.php';
 		include_once 'user.html';
 		break;
 	case 'addgroup':
+		include_once 'auth.php';
 		include_once 'group.html';
 		break;
 	case 'addincident':
+		include_once 'auth.php';
 		include_once 'incident.html';
 		break;
-		
 	default:
-
 		include_once 'login.html';
 		break;
 }
